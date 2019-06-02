3dc16c659ae3c65dd30252799998f62f5ebacb72
Janne Vanhanen <janne.j.v.vanhanen@gmail.com>
2019-06-02 22:57:22 +0300

    Finished exercise 2.4-2.5

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -2,29 +2,50 @@ import React from 'react';
 import Course from './components/Course'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fucd ndamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      },
-      {
-        name: 'Redux',
-        exercises: 11
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        },
+        {
+          name: 'Redux',
+          exercises: 11
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />;
+  return (
+    <>
+     {courses.map(course => (
+       <Course key={course.name} course={course} />
+     ))}
+    </>
+  );
 }
 
 export default App;
