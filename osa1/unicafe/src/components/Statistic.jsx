9b49c7c22082176a7a7d7df1ded6ce045e16e11b
Janne Vanhanen <janne.j.v.vanhanen@gmail.com>
2019-06-02 18:35:48 +0300

    Finished exercise 1.11

--- osa1/unicafe/src/components/Statistic.jsx
+++ osa1/unicafe/src/components/Statistic.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Statistic = ({ text, value }) => (
-  <p>{text} {value}</p>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 );
 
 export default Statistic;
--- osa1/unicafe/src/components/Statistics.jsx
+++ osa1/unicafe/src/components/Statistics.jsx
@@ -6,14 +6,25 @@ const Statistics = ({ options, votes }) => (
     <h1>statistics</h1>
     {votes.length === 0 ?
       <h4>No feedback given</h4> :
-      <>
-        {options.map(o => (
-          <Statistic key={o.value} text={o.label} value={votes.filter(v => v.label === o.label).length} />
-        ))}
-        <p>all {votes.length}</p>
-        <p>average {votes.length > 0 ? (votes.reduce((sum, vote) => sum + vote.value, 0) / votes.length) : 0}</p>
-        <p>positive {votes.length > 0 ? ((votes.filter(v => v.value > 0).length / votes.length) * 100) : 0} %</p>
-      </>
+      <table>
+        <tbody>
+          {options.map(o => (
+            <Statistic key={o.value} text={o.label} value={votes.filter(v => v.label === o.label).length} />
+          ))}
+          <tr>
+            <td>all</td>
+            <td>{votes.length}</td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td>{votes.reduce((sum, vote) => sum + vote.value, 0) / votes.length}</td>
+          </tr>
+          <tr>
+            <td>positive</td>
+            <td>{(votes.filter(v => v.value > 0).length / votes.length) * 100} %</td>
+          </tr>
+        </tbody>      
+      </table>
     }
   </>
 );
