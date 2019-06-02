663b50aa717b94c668aa4786e44ae19b52123e65
Janne Vanhanen <janne.j.v.vanhanen@gmail.com>
2019-06-02 13:47:39 +0300

    Finished exercise 1.2

--- osa1/kurssitiedot/src/components/Content.jsx
+++ osa1/kurssitiedot/src/components/Content.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
+import Part from './Part';
 
 const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => (
   <>
-    <p>
-    {part1} {exercises1}
-    </p>
-    <p>
-    {part2} {exercises2}
-    </p>
-    <p>
-    {part3} {exercises3}
-    </p>
+    <Part part={part1} exercise={exercises1} />
+    <Part part={part2} exercise={exercises2} />
+    <Part part={part3} exercise={exercises3} />
   </>
 );
 
