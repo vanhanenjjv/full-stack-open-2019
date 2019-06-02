2a1467ce5b25553695f57ebcf3cfd234f21dce83
Janne Vanhanen <janne.j.v.vanhanen@gmail.com>
2019-06-02 18:17:52 +0300

    Fixed a typo

--- osa1/unicafe/src/components/Statistics.jsx
+++ osa1/unicafe/src/components/Statistics.jsx
@@ -4,7 +4,7 @@ const Statistics = ({ options, votes }) => (
   <>
     <h1>statistics</h1>
     {options.map(o => (
-      <p ey={o.value}>{o.label} {votes.filter(v => v.label === o.label).length}</p>
+      <p key={o.value}>{o.label} {votes.filter(v => v.label === o.label).length}</p>
     ))}
     <p>all {votes.length}</p>
     <p>average {votes.length > 0 ? (votes.reduce((sum, vote) => sum + vote.value, 0) / votes.length) : 0}</p>
